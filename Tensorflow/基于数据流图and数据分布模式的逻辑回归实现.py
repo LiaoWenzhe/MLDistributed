1fe143352f1b2ab3270962c9eb9cad571f1ec2d0
LiaoWenzhe <45705519+LiaoWenzhe@users.noreply.github.com>
2021-07-11 18:46:56 +0800

    Create 基于数据流图and数据分布模式的逻辑回归实现.py

---
+++ Tensorflow/基于数据流图and数据分布模式的逻辑回归实现.py
@@ -0,0 +1,26 @@
+import tensorflow as tf 
+cluster_spec = tf.train.ClusterSpec({
+"ps": ["psO :2222"],
+"worker": ["workerO :2222", "worker1:2222", "worker2:2222"]})
+Server = tf.train.Server(cluster, job_name, task_index)
+X = tf.placeholder(tf.float32, data_shape)
+y = tf.placeholder(tf.float32 label shape)
+if job_name == "ps":
+    server.join()
+elif: job_name == "worker":
+    tf.device(tf.train.replica_device_setter(cluster = cluster_spec)):
+    mnist = input_data.read_data_sets(data_dir, one_hot = True)
+    x_ = tf.placeholder(tf.float32, [None, 784))
+    W = tf.Variable(tf.random_normal((784 , 10J))
+    b = tf.Variable(tf.zeros([10)))
+    y = tf.matmul(x, W) + b
+    y_ = tf.placeholder(tf.f1oat32, [None, 10])
+    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logit(y,y_)
+    global_step = tf.Variable(O)
+    train_op = tf.train.AdagradOptimizer(O.Ol).minimize(loss, g1obal_step = global_step)
+    init_op = tf.global_variables_initializer()
+sv = tf.train.Supervisor(init_op = init_op, global_step = global_step)
+with sv.managed_session(server. target) as sess:
+    while not sv.should_stop() and step < 1000:
+        _, step = sess.run([train_op, global_step])
+sv.stop()
